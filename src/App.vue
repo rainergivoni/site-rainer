269eb5c3b977cf07aa5806dda2cde6d5adf5f3bc
Rainer Crispim Givone (BR-ICCT) <bc2g8306@cal-comp.com.br>
2023-06-27 08:59:08 -0400

    tailwind configurado

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,19 @@
 <script setup lang="ts">
-
+import BarraDeTitulo from './components/BarraDeTitulo.vue';
 </script>
 
 <template>
   <header>
-    
-
-    
+    <BarraDeTitulo/>
   </header>
 
   <main>
-    
+
   </main>
 
-<footer>
+  <footer>
 
-</footer>
-
+  </footer>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
